1ff51cf3cd78073a5bc05b35c3e08fd0a9a3775a
Deetss <dylan817@live.com>
2021-11-05 14:46:36 -0500

    remove nosense from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'static_pages#index'
 end
