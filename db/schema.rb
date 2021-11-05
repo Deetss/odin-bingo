56a415663db727024454899bfc528e983fdf5f66
Deetss <dylan817@live.com>
2021-11-05 14:04:59 -0500

    create static pages, add user devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_183921) do
+ActiveRecord::Schema.define(version: 2021_11_05_184628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.boolean "active"
+    t.bigint "user_id", null: false
+    t.bigint "squares_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["squares_id"], name: "index_boards_on_squares_id"
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "squares", force: :cascade do |t|
+    t.string "content"
+    t.boolean "filled"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_11_05_183921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boards", "users"
 end
