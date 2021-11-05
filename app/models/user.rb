d57f6b2ce23509f17b66f742c541d96f9a045334
Deetss <dylan817@live.com>
2021-11-05 15:02:53 -0500

    fix image attachment

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,11 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      image = open(auth.info.image)
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.image.attach(io: auth.info.avatar, filename: "user_avatar.jpg") # assuming the user model has an image
+      user.image.attach(io: image, filename: "user_avatar.jpg") # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
